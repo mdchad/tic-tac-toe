8341796d06e0c9bb905e585b8bbfa2b6b85efc13
Irsyad <irsyad.muhd@gmail.com>
2016-08-22 01:38:25 +0800

    changes to javascript

--- js/app.js
+++ js/app.js
@@ -1,125 +1,81 @@
-var turn = 'X';
-var score = {
-    'X': 0,
-    'O': 0
-};
-var gridValue = 0;
+function startGame () {
 
-function fnLoad() {
-    var select = document.getElementById("grid");
-    for (i = 3; i <= 100; i += 1) {
-        var option = document.createElement('option');
-        select.options[select.options.length] = new Option(i + ' X ' + i, i);
-    }
-
-    addEvent(document.getElementById("game"), "click", fnChoose);
-
-    fnNewGame();
+  for (var i = 1; i <= 9; i++) {
+     clearBox(i);
+  }
+     document.turn = "X" ;
+  if (Math.random() < 0.5) {
+    document.turn = "O" ;
+  }
+  document.winner = null;
+  setMessage(document.turn + " gets to start");
 }
 
-function addEvent(element, eventName, callback) {
-
-    if (element.addEventListener) {
-        element.addEventListener(eventName, callback, false);
-    } else if (element.attachEvent) {
-        element.attachEvent("on" + eventName, callback);
-    }
+function setMessage(msg) {
+  document.getElementById('message').innerText = msg;
 }
 
-function fnChoose(e) {
-    if (e.target && e.target.nodeName == "TD") {
-        var targetElement = document.getElementById(e.target.id);
-        var prevTurn;
-        if ((targetElement.className).indexOf("disabled") == -1) {
-            targetElement.innerHTML = turn;
-            targetElement.classList.add('disabled');
-            targetElement.classList.add(turn);
-            score[turn] += 1;
-            prevTurn = turn;
-            turn = turn === "X" ? "O" : "X";
-            if (fndecide(targetElement, prevTurn)) {
-                alert(prevTurn + ' has won the game');
-                fnNewGame();
-            } else if ((score['X'] + score['O']) == (gridValue * gridValue)) {
-                alert('Draw!');
-                fnNewGame();
-            }
-        }
-    }
+function nextMove(square) {
+  if (document.winner != null) {
+    setMessage(document.winner + " already won the game");
+  }
+  else if (square.innerText == ""){
+    square.innerText = document.turn;
+    switchTurn();
+  } else {
+    setMessage("That square is already used");
+  }
 }
 
-function fndecide(targetElement, prevTurn) {
-    var UL = document.getElementById('game');
-    var elements, i, j, cnt;
-    if (score[prevTurn] >= gridValue) {
-        var classes = targetElement.className.split(/\s+/);
-        for (i = 0; i < classes.length; i += 1) {
-            cnt = 0;
-            if (classes[i].indexOf('row') !== -1 || classes[i].indexOf('col') !== -1 || classes[i].indexOf('dia') !== -1) {
-                elements = UL.getElementsByClassName(classes[i]);
-                for (j = 0; j < elements.length; j += 1) {
-                    if (elements[j].innerHTML == prevTurn) {
-                        cnt += 1;
-                    }
-                }
-                if (cnt == gridValue) {
-                    return true;
-                }
-            }
-        }
-    }
-    return false;
+function switchTurn() {
+   if (checkForWinner(document.turn)) {
+     setMessage("Congratulations, " + document.turn + " ! You win!");
+     document.winner = document.turn;
+   }
+   else if (document.turn == "X") {
+       document.turn = "O";
+       setMessage("It's " + document.turn + " 's turn")
+
+   } else {
+      document.turn = "X";
+      setMessage("It's " + document.turn + " 's turn")
+
+   }
 }
 
-function fnNewGame() {
-    var gameUL = document.getElementById("game");
-    if (gameUL.innerHTML !== '') {
-        gameUL.innerHTML = null;
-        score = {
-            'X': 0,
-            'O': 0
-        };
-        turn = 'X';
-        gridValue = 0;
-    }
-    var select = document.getElementById("grid");
-    gridValue = select.options[select.selectedIndex].value;
-    var i, j, li, k = 0,
-        classLists;
-    var gridAdd = +gridValue + 1;
+function checkForWinner(move) {
+  var result= false;
+  if (checkRow(1, 2, 3, move) ||
+      checkRow(4, 5, 6, move) ||
+      checkRow(7, 8, 9, move) ||
+      checkRow(1, 4, 7, move) ||
+      checkRow(2, 5, 8, move) ||
+      checkRow(3, 6, 9, move) ||
+      checkRow(1, 5, 9, move) ||
+      checkRow(3, 5, 7, move)) {
 
-    for (i = 1; i <= gridValue; i += 1) {
-        tr = document.createElement('tr');
-        for (j = 1; j <= gridValue; j += 1) {
-            k += 1;
-            li = document.createElement('td');
-            li.setAttribute("id", 'li' + k);
+      result = true;
+      return result;
+      }
 
-            classLists = 'td row' + i + ' col' + j;
-
-            if (i === j) {
-                classLists = 'td row' + i + ' col' + j + ' dia0';
-            }
-
-            if ((i + j) === gridAdd) {
-                classLists = 'td row' + i + ' col' + j + ' dia1';
-            }
-
-            if (!isEven(gridValue) && (Math.round(gridValue / 2) === i && Math.round(gridValue / 2) === j))
-                classLists = 'td row' + i + ' col' + j + ' dia0 dia1';
-
-            li.className = classLists;
-            tr.appendChild(li);
-
-        }
-        gameUL.appendChild(tr);
-    }
 }
 
-
-function isEven(value) {
-    if (value % 2 == 0)
-        return true;
-    else
-        return false;
+function checkRow(a, b, c, move) {
+   var result = false;
+   if (getBox(a) == move && getBox(b) == move && getBox(c) == move){
+     result = true;
+   }
+   return result;
 }
+
+function getBox(number) {
+   return document.getElementById("s" + number).innerText;
+}
+
+function clearBox(number) {
+  document.getElementById("s" + number).innerText = "";
+}
+
+// function drawCheck () {
+//   moveCount =
+// }
